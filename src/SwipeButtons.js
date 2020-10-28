44cb71c3312a34c256e01665fb0608954546d300
mirmahyar <m.mirsamadi@gmail.com>
2020-10-28 18:40:50 +0330

    added functionality to some buttons

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -7,19 +7,25 @@ import FavoriteIcon from "@material-ui/icons/Favorite";
 import FlashOnIcon from "@material-ui/icons/FlashOn";
 import IconButton from "@material-ui/core/IconButton";
 
+const clicked = () => {
+  alert("You liked this person, now Swipe right! ");
+};
+const clickedCross = () => {
+  alert("Don't like this person? That is alright ! Just swipe left ! ");
+};
 function SwipeButtons() {
   return (
     <div className="swipeButtons">
       <IconButton className="swipeButtons__repeat">
         <ReplayIcon fontsize="small" />
       </IconButton>
-      <IconButton className="swipeButtons__left">
+      <IconButton className="swipeButtons__left" onClick={() => clickedCross()}>
         <CloseIcon fontsize="large" />
       </IconButton>
-      <IconButton className="swipeButtons__star">
+      <IconButton className="swipeButtons__star" onClick={() => clicked()}>
         <StarRateIcon fontsize="large" />
       </IconButton>
-      <IconButton className="swipeButtons__right">
+      <IconButton className="swipeButtons__right" onClick={() => clicked()}>
         <FavoriteIcon fontsize="large" />
       </IconButton>
       <IconButton className="swipeButtons__lightning">
